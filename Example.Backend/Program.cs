ed4e97e40f713de9270db709d05167c00f9c565b
Mikhail Mitrofanov <mmm606@inbox.ru>
2025-02-05 15:08:38 +0300

    добавлены не загруженные тесты

---
+++ Example.Backend/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using Example.Shared;
+using mROA.Codegen;
+using mROA.Implementation;
+using mROA.Implementation.Bootstrap;
+
+
+var bootstrap = new FullMixBuilder();
+bootstrap.UseJsonSerialisation();
+bootstrap.UseNetworkGateway(new IPEndPoint(IPAddress.Loopback, 4567));
+bootstrap.UseStreamInteraction();
+bootstrap.UseBasicExecution();
+bootstrap.UseCollectableContextRepository(typeof(IPrinterFactory).Assembly);
+bootstrap.SetupMethodsRepository(new CoCodegenMethodRepository());
+bootstrap.Build();
+
+var gateway = bootstrap.GetModule<IGatewayModule>();
+
+gateway.Run();
---
+++ Example.Frontend/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using System.Text;
+using Example.Shared;
+using mROA.Codegen;
+using mROA.Implementation;
+using mROA.Implementation.Bootstrap;
+
+var mixer = new FullMixBuilder();
+
+mixer.Modules.Add(new FrontendContextRepository());
+mixer.Modules.Add(new JsonFrontendSerialisationModule());
+mixer.Modules.Add(new StreamBasedInteractionModule());
+mixer.Modules.Add(new NetworkFrontendBridge(new IPEndPoint(IPAddress.Loopback, 4567)));
+
+mixer.Build();
+var context = mixer.GetModule<FrontendContextRepository>();
+
+var factory = context.GetSingleObject(typeof(IPrinterFactory)) as IPrinterFactory;
+
+var printer = factory.Create("Test");
+var name = printer.Value.GetName();
+Console.WriteLine("Printer name : {0}", name);
+var page = printer.Value.Print("Test Page");
+var data = page.Value.GetData();
+Console.WriteLine("Data : {0}", Encoding.UTF8.GetString(data));
